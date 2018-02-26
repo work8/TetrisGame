5c00cd6248aa88832d1671412406f014cbaa9ef4
work8 <work8@naver.com>
2018-02-26 17:58:36 +0900

    Commit 26 Feb 2018

--- Tetris/Form1.cs
+++ Tetris/Form1.cs
@@ -12,14 +12,48 @@ namespace Tetris
 {
     public partial class Form1 : Form
     {
+        private const int FormX = 12;
+        private const int FormY = 24;
+
         public Form1()
         {
             InitializeComponent();
+
+            InitializeMap();
+
+            
+            
         }
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
+        #region Initialize map
+        private void InitializeMap()
         {
+            Panel mapPanel = new Panel();
+            this.Controls.Add(mapPanel);
+            mapPanel.Location = new Point(12, 12);
+            mapPanel.Size = new Size(FormX * 20, FormY * 20);
+            mapPanel.Padding = new Padding(0);
+            PictureBox[,] mapBox = new PictureBox[FormX, FormY];
 
+            for (int y = 0; y < FormY; y++)
+            { 
+                for (int x=0; x<FormX; x++)
+                    {
+                    mapBox[x, y] = new PictureBox();
+                    mapBox[x, y].Visible = false;
+                    mapPanel.Controls.Add(mapBox[x, y]);
+                    mapBox[x, y].Size = new Size(20, 20);
+                    mapBox[x, y].Image = Tetris.Properties.Resources.ShapeImage;
+                    mapBox[x, y].Location = new Point(0 + x * 20, 0 + y * 20);
+                    //mapBox[x, y].
+                    mapBox[x, y].Visible = true;
+                }
+            }
+        
         }
+
+        #endregion
+
+
     }
 }
